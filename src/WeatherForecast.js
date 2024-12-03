7b1ce9b75c4fbf00d1f0d0711ca13b7e59a7a12e
g-ndlovu <langathiwendlovu97@gmail.com>
2024-12-03 18:32:32 +0200

    Added WeatherForecastDay.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
-import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
@@ -31,11 +30,7 @@ export default function WeatherForecast(props) {
         <div className="row">
           {forecast.map(function (dailyForecast, index) {
             if (index < 5) {
-              return (
-                <div className="col" key={index}>
-                  <WeatherForecastDay data={dailyForecast} />
-                </div>
-              );
+              return <div className="col" key={index}></div>;
             } else {
               return null;
             }
