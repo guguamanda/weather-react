886ab5ce6a5e3869e23e75c7b54b4c7ced501943
g-ndlovu <langathiwendlovu97@gmail.com>
2024-12-03 18:57:14 +0200

    Debugging

--- src/App.js
+++ src/App.js
@@ -1,40 +1,21 @@
 import React from "react";
-import Weather from "./Weather";
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <div className="container">
-        <Weather defaultCity="New York" />
-
-        <footer>
-          This project was coded by{" "}
-          <a
-            href="https://github.com/guguamanda"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Gugulethu Amanda Ndlovu
-          </a>{" "}
-          and is{" "}
-          <a
-            href="https://github.com/guguamanda/weather-react"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            open-sourced on GitHub
-          </a>{" "}
-          and{" "}
-          <a
-            href="https://weather-app-react01.netlify.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            hosted on Netlify
-          </a>
-        </footer>
-      </div>
+      <h1>Weather App</h1>
+      <footer>
+        This projected was coded by{" "}
+        <a href="https://github.com/guguamanda" target="_blank">
+          Gugulethu Amanda Ndlovu
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/guguamanda/weather-react" target="_blank">
+          open-sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
