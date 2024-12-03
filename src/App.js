b72a40a1844cc2b4d36a1e56a077e7bffc74de4b
g-ndlovu <langathiwendlovu97@gmail.com>
2024-12-03 14:13:22 +0200

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,34 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="San Francisco" />
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/guguamanda"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Gugulethu Amanda Ndlovu
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/guguamanda/weather-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://weather-app-react01.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
       </div>
     </div>
   );
